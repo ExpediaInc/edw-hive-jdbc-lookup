994974ff6f60b350ba6327c1b404ba73ab2c3b50
Yaroslav Mykhaylov <yaroslav91@epuakyiw0905.kyiv.epam.com>
2013-07-05 18:12:36 +0300

    Edited caching strategy (caching ready string)

---
+++ cache-rest-ehcache/src/main/java/com/expedia/edw/cache/controller/CacheControllerRest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2013 Expedia, Inc. All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expedia.edw.cache.controller;
+
+import com.expedia.edw.cache.service.GrabberService;
+import java.util.Map;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Illya Yalovyy
+ * @author Yaroslav Mykhaylov
+ */
+@Controller
+@RequestMapping(value = "/service")
+public class CacheControllerRest {
+
+    private static final Logger logger =
+            Logger.getLogger(CacheControllerRest.class);
+    @Autowired
+    private GrabberService grabberService;
+
+    @RequestMapping(value = "/get/{schemaNameAndTableName}/{keyName}/{valueName}",
+            method = RequestMethod.GET)
+    @ResponseBody
+    public String getData(@PathVariable String schemaNameAndTableName,
+            @PathVariable String keyName, @PathVariable String valueName) {
+
+        CacheManager cache = CacheManager.getInstance();
+        Ehcache c = cache.getEhcache("myCache");
+       logger.info(c.getName());
+       logger.info(c.getWithLoader("key", c.getRegisteredCacheLoaders().get(0),"k"));
+       logger.info(c.getKeys());
+        for(String cacheName: cache.getCacheNames()){
+            logger.info(cacheName);
+        }
+        return grabberService.getData(schemaNameAndTableName, keyName, valueName);
+    }
+}
